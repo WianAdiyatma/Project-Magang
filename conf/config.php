cdd63214e9bbe43f61c24b3deff745f16c2aa478
daniaputriw <daniaputri80@gmail.com>
2023-12-16 19:06:09 +0700

    setting template dasar

---
+++ conf/config.php
@@ -0,0 +1,11 @@
+<?php 
+//connect to a database
+$koneksi = mysqli_connect("localhost", "root", "", "sdm");
+//mengecek koneksi
+// if(!$koneksi){
+//     die("Koneksi Gagal:". mysqli_connect_error());
+// }
+// else{
+//     echo "Koneksi Berhasil";
+// }
+?>
